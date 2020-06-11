e73a3ae38a9789cb1c87c14f63d4f13c20adf25f
Shivam Rajput <58025059+shiviraj@users.noreply.github.com>
2020-06-11 13:55:51 +0530

    |#6|Shiviraj|modified test

--- test/testCustomer.js
+++ test/testCustomer.js
@@ -6,15 +6,12 @@ const { setupDatabase, cleanupDatabase } = require('./fixture/db');
 describe('Customer', () => {
   describe('static page', () => {
     it('should serve the login page', async () => {
-      await request(app)
-        .get('/customer/login.html')
-        .expect(200)
-        .expect(/login/);
+      await request(app).get('/login.html').expect(200).expect(/login/);
     });
 
     it('should serve the register page', async () => {
       await request(app)
-        .get('/customer/register.html')
+        .get('/register.html')
         .expect(200)
         .expect(/Register/);
     });
